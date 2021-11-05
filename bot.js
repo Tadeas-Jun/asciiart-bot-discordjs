8ce595333a814d4c354d8cb5618074c29446d00e
Tadeas Salvatore Jun <darezar@eledris.com>
2021-11-05 09:09:40 +0100

    Initial commit

---
+++ bot.js
@@ -0,0 +1,65 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [] });
+const { token } = require('./resources/config.json');
+
+const commandTemplate = require('./resources/command_template.json');
+const artList = require('./resources/art.json');
+
+console.log('Starting bot, please give me a second.');
+client.on('ready', () => {
+
+    console.log('I am ready to send some ASCII art!');
+
+    client.user.setActivity('you be awesome (☞ﾟヮﾟ)☞', {type: "WATCHING"});
+
+    CreateCommands();
+
+});
+
+async function CreateCommands() {
+
+    console.log('Creating commands.');
+
+    let commandData = [];
+
+    artList.forEach(artObject => {
+
+        console.log(`Creating a command for: \'${artObject.name}\'.`);
+        let command = {...commandTemplate};
+        command.name = artObject.name;
+        command.description = command.description.replace('[art]', artObject.art);
+        commandData.push(command);
+
+    });
+
+    console.log(`Deploying ${commandData.length} commands.`);
+
+    await client.guilds.cache.get('434786513688199169')?.commands.set(commandData);
+
+}
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const asciiArt = artList.find(art => art.name === interaction.commandName);
+    if(!isAsciiArtValid(asciiArt)) return;
+
+    let message = interaction.options.getString('message');
+
+    const replyString = message === null ? asciiArt.art : message + ' ' + asciiArt.art;
+
+    console.log(`Sending \'${asciiArt.name}\'.`);
+    interaction.reply(replyString);
+
+});
+
+function isAsciiArtValid(artObject) {
+
+    const valid = (artObject.art !== null && artObject.art.trim().length !== 0) &&
+        (artObject.name !== null && artObject.name.trim().length !== 0);
+
+        return valid;
+
+}
+
+client.login(token);
